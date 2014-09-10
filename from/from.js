89b12c2e9eaa7a30f3c459b6df1da5cf21cc62ec
SaschaNaz <saschanaz@outlook.com>
2014-09-10 21:59:46 +0900

    first commit

---
+++ from/from.js
@@ -0,0 +1,24 @@
+﻿var ArrayExtensions;
+(function (ArrayExtensions) {
+    function from(arrayLike, mapFn, thisArg) {
+        var array = [];
+        var push;
+        if (!mapFn)
+            push = function (item, index) {
+                return array.push(item);
+            };
+        else if (thisArg === undefined)
+            push = function (item, index) {
+                return array.push(mapFn(item, index));
+            };
+        else
+            push = function (item, index) {
+                return array.push(mapFn.call(thisArg, item, index));
+            };
+        for (var i = 0; i < arrayLike.length; i++)
+            push(arrayLike[i], i);
+        return array;
+    }
+    ArrayExtensions.from = from;
+})(ArrayExtensions || (ArrayExtensions = {}));
+//# sourceMappingURL=from.js.map
