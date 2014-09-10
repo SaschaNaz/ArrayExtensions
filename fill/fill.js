eeaa1119918e51e656145232f742a5577f691546
SaschaNaz <saschanaz@outlook.com>
2014-09-10 22:15:17 +0900

    fill

---
+++ fill/fill.js
@@ -0,0 +1,8 @@
+﻿if (!Array.prototype.fill)
+    Array.prototype.fill = function (value, start, end) {
+        if (typeof start === "undefined") { start = 0; }
+        if (typeof end === "undefined") { end = this.length; }
+        for (var i = start; i < end; i++)
+            this[i] = value;
+    };
+//# sourceMappingURL=fill.js.map
---
+++ fill/fill.ts
@@ -0,0 +1,8 @@
+﻿interface Array<T> {
+    fill(value: T, start?: number, end?: number): void;
+}
+if (!Array.prototype.fill)
+    Array.prototype.fill = function (value: any, start = 0, end = this.length) {
+        for (var i = start; i < end; i++)
+            this[i] = value;
+    }
