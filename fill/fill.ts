860f3b48f60979919b4491a594babeb1e0110b4b
Kagami Sascha Rosylight <saschanaz@outlook.com>
2015-02-23 18:20:59 +0900

    Update fill.ts

--- fill/fill.ts
+++ fill/fill.ts
@@ -1,8 +1,9 @@
-﻿interface Array<T> {
-    fill(value: T, start?: number, end?: number): void;
+interface Array<T> {
+    fill(value: T, start?: number, end?: number): T[];
 }
 if (!Array.prototype.fill)
     Array.prototype.fill = function (value: any, start = 0, end = this.length) {
         for (var i = start; i < end; i++)
             this[i] = value;
+        return this;
     }
